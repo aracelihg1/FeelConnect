e2496e9fdad030ddfefd9bc13605aa4e36599f52
Araceli Hernández García <169103396+aracelihg1@users.noreply.github.com>
2025-05-15 15:58:32 -0600

    Dashboard

--- src/components/Estadisticas/Estadisticas.js
+++ src/components/Estadisticas/Estadisticas.js
@@ -5,6 +5,16 @@ const Estadisticas = () => {
   return (
     <div className="estadisticas-container">
       <h1 className="titulo-estadisticas">Estadísticas de la Comunidad</h1>
+
+      <div className="dashboard-container">
+        <iframe 
+          title="Dashboard - FeelConnect" 
+          className="powerbi-dashboard"
+          src="https://app.powerbi.com/reportEmbed?reportId=c32db836-a97e-4fa8-a196-9af8f4651ae8&autoAuth=true&ctid=4d9a6d94-182b-4529-abd2-1a97909cb9a5" 
+          frameBorder="0" 
+          allowFullScreen="true"
+        ></iframe>
+      </div>
     </div>
   );
 };
